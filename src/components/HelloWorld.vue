5f90e33d7aa3dc9403384676a56f2a7d35e8e8d6
Herson eder alvarado florez <aherson@hotmail.com>
2021-03-04 18:33:17 -0500

    segundo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,8 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3>HELLO WORDLSs</h3>
+    <h3>test funcionamie</h3>
+    g
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
